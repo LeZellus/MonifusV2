da1a5760fb3c9aa56e96fbd057954ca326eb54b4
LeZellus <matheo.zeller@gmail.com>
2025-07-04 00:58:24 +0200

    add x1000 for lots

--- src/Enum/SaleUnit.php
+++ src/Enum/SaleUnit.php
@@ -7,6 +7,7 @@ enum SaleUnit: int
     case UNIT = 1;
     case TEN = 10;
     case HUNDRED = 100;
+    case THOUSAND = 1000;
 
     public function getLabel(): string
     {
@@ -14,6 +15,7 @@ enum SaleUnit: int
             self::UNIT => 'À l\'unité (x1)',
             self::TEN => 'Par 10 (x10)',
             self::HUNDRED => 'Par 100 (x100)',
+            self::THOUSAND => 'Par 1000 (x1000)',
         };
     }
 }
